046eb31dbc1d589dbf3f871abd2dc55e26bb5e3b
MohibShaikh <mohibuddin9@gmail.com>
2026-03-14 04:11:54 +0500

    chore: bump CLI to v0.5.0 for telemetry + feedback release

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("clawvet")
   .description("Skill vetting & supply chain security for OpenClaw")
-  .version("0.4.0");
+  .version("0.5.0");
 
 program
   .command("scan")
